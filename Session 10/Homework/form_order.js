2f6e16e57a5b5d110ceab7536d237b139ecb8bf1
Thoang0203 <Thoang0203@users.noreply.github.com>
2018-10-03 18:31:24 +0700

    Session 10 (Form jquery)

--- Session 10/Homework/form_order.js
+++ Session 10/Homework/form_order.js
@@ -5,19 +5,19 @@ $('#submit').click(function(){
 		} else {
 			$('.cl'+i).html('');
 			if ($('#id3').val() > $('#id2').val()) {
-			$('.cl3').html('Số trẻ em không được lớn hơn số người lớn!');
+				$('.cl3').html('Số trẻ em không được lớn hơn số người lớn!');
 			} else {
 				$('.cl3').html('');
 			}
-			var today = new Date();
+			var today = Date.now();
 			var day_come = new Date($('#id4').val());
 			var day_leave = new Date($('#id5').val());
-			if ((day_come < today) && (day_come != "")) {
+			if (day_come < today) {
 				$('.cl4').html('Ngày đến phải lớn hơn hoặc bằng ngày hiện tại!');
 			} else {
 				$('.cl4').html('');
 			}
-			if ((day_leave < day_come) && (day_come != "") && (day_leave != "")) {
+			if (day_leave < day_come) {
 				$('.cl5').html('Ngày đi phải lớn hơn hoặc bằng ngày đến!');
 			} else {
 				$('.cl5').html('');
